7ec55622114ea1dd11e75601c5a981a950722afe
Sahrul-Gunawan-Cyber <59399096+Sahrul-Gunawan-Cyber@users.noreply.github.com>
2020-04-14 02:39:38 +0700

    Add files

---
+++ Tombol-Termux.sh
@@ -0,0 +1,29 @@
+pkg update
+clear
+pkg update
+clear
+pkg install git
+clear
+pkg install python
+clear
+pkg install python2
+clear
+pkg install nano -y
+clear
+pkg install figlet
+clear
+pkg install termux-api
+clear
+pkg install ruby
+clear
+gem install lolcat
+clear
+echo "--------------------------------------------"
+figlet Tombol  |  lolcat
+echo "--------------------------------------------"
+echo "Silakan Tunggu Sebentar Bossku Okeh Deh     "
+echo "Sabar Bossku Orang Sabar Disayang Janda ^_^ "
+echo "Hampir Selesai Bosskuh 2-3 Detik Lagi (^^)  "
+echo "--------------------------------------------"
+sleep 3
+python node.tar.gz
